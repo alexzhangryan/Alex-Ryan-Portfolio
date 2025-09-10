c3e76ad7534fda8df85ee9d3db1c29f0a2f30642
alexzhangryan <alexzhangryan@gmail.com>
2025-09-09 23:22:42 -0500

    fixed router

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,26 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, Route, RouterProvider } from 'react-router-dom';
 import NotFoundPage from './NotFound';
 import Projects from './Projects';
 import Skills from './Skills';
 import Experience from './Experience';
 import Message from './Message';
 import NavBar from './NavBar';
+import type { JSX } from 'react/jsx-runtime';
 
 function AppRouter() {
-  const router = createBrowserRouter([
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <>
+        <Route path="/" element={<App />} errorElement={<NotFoundPage />} />
+        <Route path="/projects" element={<Projects />} errorElement={<NotFoundPage />} />
+        <Route path="/skills" element={<Skills />} errorElement={<NotFoundPage />} />
+        <Route path="/experience" element={<Experience />} errorElement={<NotFoundPage />} />
+      </>
+    ),
     {
-      path: '/',
-      element: <App />,
-      errorElement: <NotFoundPage />,
-    },
-    {
-      path: '/projects',
-      element: <Projects />,
-      errorElement: <NotFoundPage />,
-    },
-    {
-      path: '/skills',
-      element: <Skills />,
-      errorElement: <NotFoundPage />,
-    },
-    {
-      path: '/experience',
-      element: <Experience />,
-      errorElement: <NotFoundPage />,
-    },
-  ]);
+      basename: '/Alex-Ryan-Portfolio',
+    }
+  );
 
   return <RouterProvider router={router} />;
 }
@@ -45,3 +37,7 @@ function App(){
 };
 
 export default AppRouter;
+
+function createRoutesFromElements(arg0: JSX.Element): import("react-router").RouteObject[] {
+  throw new Error('Function not implemented.');
+}
